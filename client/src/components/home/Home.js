e3c09ee417c80ed219f5ee29e57a48c020ba66b5
Yeison <enmanuelquiterios@gmail.com>
2021-12-05 00:41:38 -0500

    Changed Files

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    fetch("http://localhost:3000");
+  }, []);
   return (
     <div>
       <h1>Welcome To the root</h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
 
 import RouterApp from "./components/RouterApp";
 
